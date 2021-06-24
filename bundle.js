621402185d0d5284fba4297cd8a5c39b7def1cef
Fiona Ye <fiona.ye@yandex.com>
2021-06-25 02:17:05 +0800

    rslot and mode closed

--- bundle.js
+++ bundle.js
@@ -1241,39 +1241,49 @@ const svg = require('svg')
 module.exports = ({name, path, theme}) => {
     function layout(style) {
         const icon = document.createElement('i-icon')
-        const root = icon.attachShadow({mode: 'open'})
+        const root = icon.attachShadow({mode: 'closed'})
         const url = path ? path : './src/svg'
         const img = svg(`${url}/${name}.svg`)
+        const slot = document.createElement('slot')
+        slot.name = 'icon'
         styleSheet(root, style)
-        root.append(img)
+        slot.append(img)
+        root.append(slot)
         return icon
     }
     // insert CSS style
     const customStyle = theme ? theme.style : ''
     // set CSS variables
     if (theme && theme.props) {
-        var { fill, size } = theme.props
+        var { fill, fillHover, size } = theme.props
     }
     const style = `
     :host(i-icon) {
+        --size: ${size ? size : '20px'};
+        --fill: ${fill ? fill : 'var(--primary-color)'};
+        --fillHover: ${fillHover ? fillHover : 'var(--color-greyA2)'};
+        display: block;
+    }
+    slot[name='icon'] {
         display: grid;
         justify-content: center;
         align-items: center;
     }
-    :host(i-icon) span {
-        --size: ${size ? size : '20px'};
+    slot[name='icon'] span {
         display: block;
         width: var(--size);
         height: var(--size);
     }
-    :host(i-icon) svg {
+    slot[name='icon'] svg {
         width: 100%;
         height: auto;
     }
-    :host(i-icon) g {
-        --fill: ${fill ? fill : 'var(--primary-color)'};
+    slot[name='icon'] g {
         fill: hsl(var(--fill));
         transition: fill .3s ease-in-out;
+    }
+    slot[name='icon']:hover g {
+        fill: hsl(var(--fillHover));
     }
     ${customStyle}
     `
